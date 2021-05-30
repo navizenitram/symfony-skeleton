0b671780d7f4dd39ec35f8ac9bdffea204ba3747
Ivan Martinez <ivan@degustabox.com>
2021-05-30 11:43:01 +0200

    Elevator test

---
+++ src/Application/Simulator/Simulator.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Application\Simulator;
+
+class Simulator
+{
+
+}
---
+++ tests/App/Application/Simulator/SimulatorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Application\Simulator;
+
+use PHPUnit\Framework\TestCase;
+
+class SimulatorTest extends TestCase
+{
+    public function testIsClass() {
+        $request = new SimulatorRequest();
+    }
+}
