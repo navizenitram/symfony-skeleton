79acc561afeb2d23f85b216ae8d16b55470a9414
Ivan Martinez <ivan@degustabox.com>
2021-05-30 11:50:47 +0200

    Elevator test - test working again

--- src/Application/Simulator/Simulator.php
+++ src/Application/Simulator/Simulator.php
@@ -1,8 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Application\Simulator;
 
-class Simulator
+use DateInterval;
+use DatePeriod;
+
+final class Simulator
 {
 
+    public function execute(SimulatorRequest $request): SimulatorResponse
+    {
+        $response = new SimulatorResponse();
+        $report = [];
+
+        $interval = DateInterval::createFromDateString('1 minute');
+        $period = new DatePeriod($request->getStartTime(), $interval, $request->getEndTime());
+
+        foreach ($period as $dt) {
+            echo $dt->format("H:i\n");
+        }
+
+
+        return $response;
+    }
 }
--- src/Application/Simulator/SimulatorRequest.php
+++ src/Application/Simulator/SimulatorRequest.php
@@ -1,8 +1,80 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Application\Simulator;
 
-class SimulatorRequest
+use DateTime;
+
+final class SimulatorRequest
 {
+    private $startTime;
+    private $endTime;
+    private $elevators = [];
+    private $elevatorSequences = [];
+
+    /**
+     * @return DateTime
+     */
+    public function getStartTime(): DateTime
+    {
+        return $this->startTime;
+    }
+
+    /**
+     * @param DateTime $startTime
+     */
+    public function setStartTime(DateTime $startTime): void
+    {
+        $this->startTime = $startTime;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getEndTime(): DateTime
+    {
+        return $this->endTime;
+    }
+
+    /**
+     * @param DateTime $endTime
+     */
+    public function setEndTime(DateTime $endTime): void
+    {
+        $this->endTime = $endTime;
+    }
+
+    /**
+     * @return array
+     */
+    public function getElevators(): array
+    {
+        return $this->elevators;
+    }
+
+    /**
+     * @param Elevator $elevator
+     */
+    public function addElevator(Elevator $elevator): void
+    {
+        $this->elevators[] = $elevator;
+    }
+
+    /**
+     * @return array
+     */
+    public function getElevatorSequences(): array
+    {
+        return $this->elevatorSequences;
+    }
+
+    /**
+     * @param ElevatorSequence $elevatorSequence
+     */
+    public function addElevatorSequence(ElevatorSequence $elevatorSequence): void
+    {
+        $this->elevatorSequences[] = $elevatorSequence;
+    }
 
 }
